b6cb6349025e0057385abc893f4a6f0d8288e236
Loc Tran <mrlocspoc@gmail.com>
2023-12-22 22:22:58 +0700

    10. Build Cypress Basic test and run from test Runner complete

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,1 +1,7 @@
 // cypress - Spec
+
+describe('My First Test Suite', function () {
+	it('My first test case', function () {
+		cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+	});
+});
