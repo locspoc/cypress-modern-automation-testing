b6cc370c75f9d69286bb57d756c6c6e775b985d6
Loc Tran <mrlocspoc@gmail.com>
2023-12-23 21:05:52 +0700

    17. Handling Invisible elements with Cypress by understanding logs

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -7,6 +7,6 @@ describe('My First Test Suite', function () {
 		cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
 		cy.get('.search-keyword').type('ca');
 		cy.wait(2000);
-		cy.get('.product').should('have.length', 5);
+		cy.get('.product:visible').should('have.length', 4);
 	});
 });
