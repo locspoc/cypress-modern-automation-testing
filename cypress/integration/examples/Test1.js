7850d7d5734e3511428981cea88329a92ee587c4
Loc Tran <mrlocspoc@gmail.com>
2023-12-23 18:09:20 +0700

    15. Cypress inbuilt plugin in testRunner to generate locators complete

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,7 +1,10 @@
 // cypress - Spec
 
+/// <reference types="Cypress" />
+
 describe('My First Test Suite', function () {
 	it('My first test case', function () {
 		cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+		cy.get('.search-keyword').type('ca');
 	});
 });
