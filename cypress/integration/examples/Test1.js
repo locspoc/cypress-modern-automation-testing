f967b04446e18f424135a09b927369158d943c49
Loc Tran <mrlocspoc@gmail.com>
2023-12-23 20:58:23 +0700

    16. Basic Assertion in writing the tests with Cypress complete

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -6,5 +6,7 @@ describe('My First Test Suite', function () {
 	it('My first test case', function () {
 		cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
 		cy.get('.search-keyword').type('ca');
+		cy.wait(2000);
+		cy.get('.product').should('have.length', 5);
 	});
 });
