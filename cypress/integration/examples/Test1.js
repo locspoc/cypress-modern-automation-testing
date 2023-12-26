3964c962e6228db60c96fc5ebc1baaddb22fde17
Loc Tran <mrlocspoc@gmail.com>
2023-12-26 17:35:00 +0700

    19. Understanding get and find commands in Cypress complete

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -7,6 +7,15 @@ describe('My First Test Suite', function () {
 		cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
 		cy.get('.search-keyword').type('ca');
 		cy.wait(2000);
+		cy.get('.product').should('have.length', 5);
 		cy.get('.product:visible').should('have.length', 4);
+		// Parent Child
+		cy.get('.products').find('.product').should('have.length', 4);
+		cy.get(':nth-child(3) > .product-action > button').click();
+		cy.get('.products')
+			.find('.product')
+			.eq(2)
+			.contains('ADD TO CART')
+			.click();
 	});
 });
