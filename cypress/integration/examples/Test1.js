d7c37b9e8f1a4c72a8755cad27215e5e56f95341
Loc Tran <mrlocspoc@gmail.com>
2023-12-26 18:25:41 +0700

    21. Grabbing the text for validators using cypress text command complete

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -9,7 +9,7 @@ describe('My First Test Suite', function () {
 		cy.wait(2000);
 		cy.get('.product').should('have.length', 5);
 		cy.get('.product:visible').should('have.length', 4);
-		// Parent Child
+		// Parent Child Chaining
 		cy.get('.products').find('.product').should('have.length', 4);
 		cy.get(':nth-child(3) > .product-action > button').click();
 		cy.get('.products')
@@ -17,5 +17,13 @@ describe('My First Test Suite', function () {
 			.eq(2)
 			.contains('ADD TO CART')
 			.click();
+		cy.get('.products')
+			.find('.product')
+			.each(($el, index, $list) => {
+				const textVeg = $el.find('h4.product-name').text();
+				if (textVeg.includes('Cashews')) {
+					cy.wrap($el).find('button').click();
+				}
+			});
 	});
 });
