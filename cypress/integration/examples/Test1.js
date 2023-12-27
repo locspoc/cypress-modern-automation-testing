50c48b23ea6599b5edbc4b30678e6a0df3035d41
Loc Tran <mrlocspoc@gmail.com>
2023-12-27 18:37:13 +0700

    24. Handling Async promises with Cypress complete

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -10,14 +10,18 @@ describe('My First Test Suite', function () {
 		cy.get('.product').should('have.length', 5);
 		cy.get('.product:visible').should('have.length', 4);
 		// Parent Child Chaining
-		cy.get('.products').find('.product').should('have.length', 4);
+		cy.get('.products').as('productLocator');
+		cy.get('@productLocator').find('.product').should('have.length', 4);
 		cy.get(':nth-child(3) > .product-action > button').click();
-		cy.get('.products')
+		cy.get('@productLocator')
 			.find('.product')
 			.eq(2)
 			.contains('ADD TO CART')
-			.click();
-		cy.get('.products')
+			.click()
+			.then(function () {
+				console.log('sf');
+			});
+		cy.get('@productLocator')
 			.find('.product')
 			.each(($el, index, $list) => {
 				const textVeg = $el.find('h4.product-name').text();
@@ -25,6 +29,9 @@ describe('My First Test Suite', function () {
 					cy.wrap($el).find('button').click();
 				}
 			});
+		// Assert if logo text is correctly displayed
+		cy.get('.brand').should('have.text', 'GREENKART');
+		// This is to print in logs
 		const logo = cy.get('.brand').then(function (logoElement) {
 			cy.log(logoElement.text());
 		});
