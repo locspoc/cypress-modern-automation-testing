dc9a5d49b8f314467e3f26c3be4cb7b615234549
Loc Tran <mrlocspoc@gmail.com>
2023-12-28 14:02:08 +0700

    30. Handling Dynamic dropdowns with each command iteration complete

--- cypress/integration/examples/Test3.js
+++ cypress/integration/examples/Test3.js
@@ -12,7 +12,16 @@ describe('My Third Test Suite', function () {
 		cy.get('#checkBoxOption1').uncheck().should('not.be.checked');
 		cy.get('input[type="checkbox"]').check(['option2', 'option3']);
 
-		// Static Dropdown
+		// Static Dropdowns
 		cy.get('select').select('option2').should('have.value', 'option2');
+
+		// Dynamic Dropdowns
+		cy.get('#autocomplete').type('ind');
+		cy.get('.ui-menu-item div').each(($el, index, $list) => {
+			if ($el.text() === 'India') {
+				cy.wrap($el).click();
+			}
+		});
+		cy.get('#autocomplete').should('have.value', 'India');
 	});
 });
