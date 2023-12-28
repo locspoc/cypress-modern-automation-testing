89d5df58ff9fbf0432a2a65352290614ff7290c3
Loc Tran <mrlocspoc@gmail.com>
2023-12-28 13:44:02 +0700

    30. Handling static dropdowns using select command with Cypress complete

--- cypress/integration/examples/Test3.js
+++ cypress/integration/examples/Test3.js
@@ -3,6 +3,7 @@
 
 describe('My Third Test Suite', function () {
 	it('My third test case', function () {
+		// Check boxes
 		cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
 		cy.get('#checkBoxOption1')
 			.check()
@@ -10,5 +11,8 @@ describe('My Third Test Suite', function () {
 			.and('have.value', 'option1');
 		cy.get('#checkBoxOption1').uncheck().should('not.be.checked');
 		cy.get('input[type="checkbox"]').check(['option2', 'option3']);
+
+		// Static Dropdown
+		cy.get('select').select('option2').should('have.value', 'option2');
 	});
 });
