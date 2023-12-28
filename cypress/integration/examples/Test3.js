444ad5159915f69bdd5f23759b971116308f6216
Loc Tran <mrlocspoc@gmail.com>
2023-12-28 13:34:55 +0700

    28. How to verify and automate check boxes with Cypress complete

---
+++ cypress/integration/examples/Test3.js
@@ -0,0 +1,14 @@
+// Intellisense autocorrect, tool tips etc
+/// <reference types="Cypress" />
+
+describe('My Third Test Suite', function () {
+	it('My third test case', function () {
+		cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
+		cy.get('#checkBoxOption1')
+			.check()
+			.should('be.checked')
+			.and('have.value', 'option1');
+		cy.get('#checkBoxOption1').uncheck().should('not.be.checked');
+		cy.get('input[type="checkbox"]').check(['option2', 'option3']);
+	});
+});
