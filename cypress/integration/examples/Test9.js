64a007d4d85d5844ca1b4f16082fd1f1f8516693
Loc Tran <mrlocspoc@gmail.com>
2023-12-29 16:45:47 +0700

    50. Modifying existing tests into Page object pattern as per Cypress standards complete

--- cypress/integration/examples/Test9.js
+++ cypress/integration/examples/Test9.js
@@ -1,5 +1,6 @@
 // Testing Framework
 import HomePage from '../pageObjects/HomePage';
+import ProductsPage from '../pageObjects/ProductsPage';
 
 // Intellisense autocorrect, tool tips etc
 /// <reference types="Cypress" />
@@ -13,27 +14,22 @@ describe('My Nineth Test Suite', function () {
 	});
 	it('Test case 1', function () {
 		const homePage = new HomePage();
+		const productsPage = new ProductsPage();
 		cy.visit('https://rahulshettyacademy.com/angularpractice/');
-		cy.get('input[name="name"]:nth-child(2)').type(this.data.name);
-		cy.get('select').select(this.data.gender);
+		homePage.getEditBox().type(this.data.name);
+		homePage.getGender().select(this.data.gender);
 		// Two way binding example input matches name
-		cy.get(':nth-child(4) > .ng-untouched').should(
-			'have.value',
-			this.data.name
-		);
+		homePage.getTwoWayDataBinding().should('have.value', this.data.name);
 		// Min Length is 2
-		cy.get('input[name="name"]:nth-child(2)').should(
-			'have.attr',
-			'minlength',
-			'2'
-		);
+		homePage.getEditBox().should('have.attr', 'minlength', '2');
 		// Entrepreneur option box is disabled
-		cy.get('#inlineRadio3').should('be.disabled');
+		homePage.getEntrepreneur().should('be.disabled');
 		cy.pause();
-		cy.get(':nth-child(2) > .nav-link').click();
+		homePage.getShopTab().click();
 		// Array and Custom Command
 		this.data.productName.forEach((product) => {
 			cy.selectProduct(product);
 		});
+		productsPage.getCheckoutButton().click();
 	});
 });
