cad8ccbf58708c674b3689f1c56a88c2bcefe9f0
Loc Tran <mrlocspoc@gmail.com>
2023-12-29 12:09:31 +0700

    44. Understand how fixtures work in driving data complete

--- cypress/integration/examples/Test9.js
+++ cypress/integration/examples/Test9.js
@@ -4,9 +4,15 @@
 /// <reference types="Cypress" />
 
 describe('My Nineth Test Suite', function () {
+	before(function () {
+		// Run once before all tests in the block
+		cy.fixture('example').then(function (data) {
+			this.data = data;
+		});
+	});
 	it('Test case 1', function () {
 		cy.visit('https://rahulshettyacademy.com/angularpractice/');
-		cy.get('input[name="name"]:nth-child(2)').type('Bob');
-		cy.get('select').select('Male');
+		cy.get('input[name="name"]:nth-child(2)').type(this.data.name);
+		cy.get('select').select(this.data.gender);
 	});
 });
