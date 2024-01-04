1eb03398b8bf4bc626e8eef1d7a854f7625ecda0
Loc Tran <mrlocspoc@gmail.com>
2024-01-04 11:53:24 +0700

    76. Integration Testing with Front end and Back End response validation assertions complete

--- cypress/integration/examples/BDD/library/libraryStepDef.js
+++ cypress/integration/examples/BDD/library/libraryStepDef.js
@@ -20,14 +20,27 @@ When('I send a reqeust for lirbary info', () => {
 					isbn: 'RSU',
 					aisle: '2301',
 				},
+				{
+					book_name: 'RestAssured with Java',
+					isbn: 'RSU',
+					aisle: '2301',
+				},
 			],
 		}
 	).as('bookretrievals');
 	cy.get("button[class='btn btn-primary']").click();
-	cy.wait('@bookretrievals');
+	cy.wait('@bookretrievals').then(({ request, response }) => {
+		cy.log(response);
+		cy.log(response.body);
+		// const length = response.body.length + 1;
+		cy.get('tr').should('have.length', response.body.length + 1);
+	});
 });
 
 Then('Receive mock data', () => {
-	cy.get('p').should('have.text', 'Oops only 1 Book available');
-	cy.get('tbody tr th').should('have.text', '1');
+	// cy.get('p').should('have.text', 'Oops only 1 Book available');
+	cy.get('tbody tr').should('have.length', '2');
+	// cy.get('tbody tr th').should('have.text', '1');
+	// cy.get('tbody tr th').should('have.text', '2');
+	// length of response array = number of rows in table
 });
